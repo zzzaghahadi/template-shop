24ce6176f048197e952d49895fdbdac1214f49af
sh.aghahadi <z77aghahadi@yahoo.com>
2022-06-27 01:24:22 +0430

    change card size

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let slideIndex = 1 ;
+function setSlide(input,index){
+    slideIndex = index;
+    let item = document.querySelector(`#${input}`)
+    let slides = [...document.querySelector('.slides').children] ;
+    slides.forEach((element)=>{
+        element.classList.remove('active');
+    })
+    item.classList.add('active');
+}
+
